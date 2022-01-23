55dc4858b1ff29def8c8ba8fa3a55cd3a810d00e
Stephen Leyva <sleyva1297@gmail.com>
2022-01-23 01:41:32 -0800

    file reading

--- TCP/src/main.rs
+++ TCP/src/main.rs
@@ -1,5 +1,6 @@
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
+use tokio::fs::File;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -13,6 +14,10 @@ async fn main() -> tokio::io::Result<()> {
             match listener.accept().await {
                 Ok((mut socket, addr)) => {
                     println!("new client: {:?}", addr);
+                    let mut buffer = Vec::new();
+
+                    let read = socket.read_to_end(&mut buffer).await.unwrap();
+                    println!("read from client: {} bytes", read);
                     socket.write_all(b"message recieved").await.unwrap();
                 }
                 Err(e) => println!("couldn't get client: {:?}", e),
@@ -22,6 +27,7 @@ async fn main() -> tokio::io::Result<()> {
 
     enum Command {
         Send(String, String),
+        File(String, String),
         Err(String),
     }
 
@@ -30,10 +36,11 @@ async fn main() -> tokio::io::Result<()> {
             let split_input: Vec<&str> = input.split(" ").collect();
             let command = split_input[0].to_owned();
             let address = split_input[1].to_owned();
-            let message = split_input[1..].join(" ").to_owned();
+            let message = split_input[2..].join(" ").to_owned();
 
             return match command.to_lowercase().as_str() {
                 "send" => Self::Send(address, message),
+                "send_file" => Self::File(address, message),
                 _ => Self::Err(input),
             };
         }
@@ -45,8 +52,16 @@ async fn main() -> tokio::io::Result<()> {
         match readline {
             Ok(line) => match Command::from(line) {
                 Command::Send(address, message) => {
-                    let mut connection = TcpStream::connect("127.0.0.1:5050").await?;
+                    let mut connection = TcpStream::connect(address).await?;
                     connection.write_all(message.as_bytes()).await?;
+                }
+                Command::File(address, file_path) => {
+                    let mut file = File::open(file_path).await?;
+                    let mut connection = TcpStream::connect(address).await?;
+                    let mut buffer = Vec::new();
+                    let read = file.read_to_end(&mut buffer).await?;
+                    println!("Sending {} bytes to client", read);
+                    connection.write_all(&mut buffer).await?;
                 }
                 Command::Err(input) => println!("did not recognize input"),
             },
